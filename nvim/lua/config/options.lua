c054387ee5ddb67441bd4f3a5fae5007919bec34
Rachit Kumar Verma <rachitverma1122@gmail.com>
2024-10-28 21:01:04 +0530

    My nvim config

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,23 @@
+local options = {
+	number = true,
+	relativenumber = true,
+	tabstop = 4,
+	shiftwidth = 4,
+	expandtab = true,
+	cursorline = true,
+	cursorcolumn = true,
+	backup = false,
+	hlsearch = true,
+	scrolloff = 10,
+	smartcase = true,
+	smartindent = true,
+	softtabstop = 2,
+	wildmenu = true,
+	writebackup = false,
+	clipboard = 'unnamedplus',
+	wrap = false,
+}
+
+for k, v in pairs (options) do
+	vim.opt[k] = v
+end
